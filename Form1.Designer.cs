3dc36d37dcfebab695e5ae241e74834240990e8c
Harry <m.tsubasa0@gmail.com>
2019-01-03 11:22:37 +0900

    プロジェクト ファイルを追加します。

---
+++ Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace WindowsFormsApp4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Sukima = new System.Windows.Forms.PictureBox();
+            this.Player = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.Gameover = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.scoreLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Sukima)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Player)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Orange;
+            this.pictureBox1.Location = new System.Drawing.Point(-19, 174);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(888, 50);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Sukima
+            // 
+            this.Sukima.BackColor = System.Drawing.SystemColors.Control;
+            this.Sukima.Location = new System.Drawing.Point(276, 174);
+            this.Sukima.Name = "Sukima";
+            this.Sukima.Size = new System.Drawing.Size(180, 50);
+            this.Sukima.TabIndex = 1;
+            this.Sukima.TabStop = false;
+            // 
+            // Player
+            // 
+            this.Player.BackColor = System.Drawing.Color.LimeGreen;
+            this.Player.Location = new System.Drawing.Point(344, 377);
+            this.Player.Name = "Player";
+            this.Player.Size = new System.Drawing.Size(24, 30);
+            this.Player.TabIndex = 2;
+            this.Player.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 20;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Gameover
+            // 
+            this.Gameover.AutoSize = true;
+            this.Gameover.Font = new System.Drawing.Font("MS UI Gothic", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Gameover.ForeColor = System.Drawing.Color.Red;
+            this.Gameover.Location = new System.Drawing.Point(242, 269);
+            this.Gameover.Name = "Gameover";
+            this.Gameover.Size = new System.Drawing.Size(244, 40);
+            this.Gameover.TabIndex = 3;
+            this.Gameover.Text = "GAME OVER";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("メイリオ", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button1.Location = new System.Drawing.Point(617, 32);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 41);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "START";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("メイリオ", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(18, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 30);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "SCORE:";
+            // 
+            // scoreLabel
+            // 
+            this.scoreLabel.AutoSize = true;
+            this.scoreLabel.Font = new System.Drawing.Font("MS UI Gothic", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.scoreLabel.Location = new System.Drawing.Point(116, 51);
+            this.scoreLabel.Name = "scoreLabel";
+            this.scoreLabel.Size = new System.Drawing.Size(42, 20);
+            this.scoreLabel.TabIndex = 6;
+            this.scoreLabel.Text = "999";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(770, 450);
+            this.Controls.Add(this.scoreLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Gameover);
+            this.Controls.Add(this.Player);
+            this.Controls.Add(this.Sukima);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "突破ゲーム";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Sukima)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Player)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox Sukima;
+        private System.Windows.Forms.PictureBox Player;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label Gameover;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label scoreLabel;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        Point Cpos;
+        Boolean shotFlg; //true :発射処理
+        Boolean hitFlg; //true  :当たり
+        long score; //スコア
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Playerの上移動
+        private void movePlayer()
+        {
+            if (hitFlg)
+            {
+                return; //プレイヤーの動きをとめる
+            }
+
+            Player.Top -= 12;
+            if ( Player.Top < (0 - Player.Height) )
+            {
+                score += 10; //画面上部まで来たのでスコア加算
+                scoreLabel.Text = score.ToString();
+                Sukima.Width -= 5; //すき間を狭くする
+                initPlayer(); //Playerの初期化
+            }
+
+            long pH = Player.Height;
+            long pW = Player.Width;
+            long sH = Sukima.Height;
+            long sW = Sukima.Width;
+
+            if ((Player.Top < Sukima.Top + sH) && (Player.Top + pH > Sukima.Top))
+              {
+                if ((Player.Left < Sukima.Left) || (Player.Left + pW > Sukima.Left + sW))
+                    {
+                        hitFlg = true;
+                        button1.Enabled = true; //開始ボタンを有効にする
+                        Gameover.Show(); //ゲームオーバーを表示する
+                    }
+             }
+        }
+
+        //すき間の横移動
+        private void moveSukima()
+        {
+            if (hitFlg)
+            {
+                return;
+            }
+
+            Sukima.Left += 4;
+            if (Sukima.Left > Width)
+            {
+                Sukima.Left = -Sukima.Width;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Cpos = PointToClient(Cursor.Position);
+            moveSukima(); //すき間の横移動
+
+            if (shotFlg)
+            {
+                movePlayer(); //Playerの上移動
+                return; //timer1_Tickから抜ける
+            }
+
+            if (Cpos.X < 0)
+            {
+                Cpos.X = 0;
+            }
+            if (Cpos.X >  Width - Player.Width)
+            {
+                Cpos.X = Width - Player.Width;
+            }
+           
+            Player.Left = Cpos.X;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            initGame();
+        }
+
+        //Gameの初期化
+        private void initGame()
+        {
+            hitFlg = false; //false:はずれ
+            Gameover.Hide(); //ゲームオーバーを隠す
+            button1.Enabled = false; //開始ボタンを無効にする
+            score = 0;  //スコアのクリア
+            scoreLabel.Text = "0";
+            Sukima.Width = 80; //すき間の幅
+            initPlayer();
+        }
+
+        //Playerの初期化
+        private void initPlayer()
+        {
+            Player.Top = Height - (Player.Height * 2);
+            Player.Left = Cpos.X;
+            shotFlg = false; //false:発射していない
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            shotFlg = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            initGame();
+        }
+    }
+}
